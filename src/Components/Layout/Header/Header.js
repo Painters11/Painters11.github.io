2ada6d827d102d7f013b38de5f6918a2f11ba7dc
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-17 15:35:52 -0500

    Altered Header for react hooks

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -5,19 +5,22 @@ import Container from 'react-bootstrap/Container'
 
 
 
-const Header = () => (
-<>
-  <Navbar bg="primary" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto justify-content-center">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">About</Nav.Link>
-      <Nav.Link href="#pricing">Paint</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-</>
-)
+function Header() {
+  
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto justify-content-center">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">About</Nav.Link>
+          <Nav.Link href="#pricing">Paint</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
 
 export default Header
