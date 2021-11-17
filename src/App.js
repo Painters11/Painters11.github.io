e8aba5cba7c9145bc6b72534f90f94884c3a766f
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-17 17:36:26 -0500

    Working on navigation and the functioning of the Home page. Hitting some snags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react'
+// import Route from 'react-router-dom'
 import Layout from './Components/Layout/layout.js'
+// import Home from './Components/pages/home'
 
 const App = props => (
   <Fragment>
     <Layout />
+    {/* <Route exact path='/' component={Home} />*/}
   </Fragment>
 )
 
--- src/Components/pages/home.jsx
+++ src/Components/pages/home.jsx
@@ -7,3 +7,5 @@ function Home(props) {
     </Fragment>
   )
 }
+
+export default Home
