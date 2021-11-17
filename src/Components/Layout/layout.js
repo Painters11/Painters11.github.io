6ca3c0efea9be61859c442911b020d29d1ac54a8
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-17 14:34:14 -0500

    created a Layouts folder with a layout.js file for structure

---
+++ src/Components/Layout/layout.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+import Header from './Header/Header.js'
+
+function Layout(props) {
+  
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default Layout
