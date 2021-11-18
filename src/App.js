49d4ce4d3faec724ed00dd5cf7876cd8f9c3a43d
greg goldman <gregory.goldman97@gmail.com>
2021-11-18 16:09:25 -0500

    Change the line component name to Line

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react'
 import Header from './Components/Header/Header.js'
-// import Canvas from './Components/Canvas/Canvas.js'
+// import Line from './Components/Canvas/Line.js'
 // import Rectangle from './Components/Canvas/Rectangle.js'
-import Circle from './Components/Canvas/Circle.js'
+// import Circle from './Components/Canvas/Circle.js'
 
 function App() {
     return (
 
       <Fragment>
         <Header />
-        {/* <Canvas /> */}
+        {/* <Line /> */}
         {/* <Rectangle /> */}
-        <Circle />
+        {/* <Circle /> */}
       </Fragment>
     
 
