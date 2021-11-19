4b631a70ba50e2c0d72fe07065e3fffbebee042b
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-19 16:22:37 -0500

    merge conflict

---
+++ src/Components/shared/Header/index.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+
+
+const Header = () => (
+<>
+  <Navbar bg="primary" variant="dark">
+    <Container>
+      <Nav className="me-auto justify-content-center">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/canvas">Draw</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+</>
+)
+
+export default Header
