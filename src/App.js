23fb4ab7a2fd4631ffe991974b80e06aceb677ed
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-19 06:42:38 -0500

    Re-added the lost canvas code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/page/home/home.jsx'
-import Canvas from './components/page/canvas/Canvas.js'
+import Home from './Components/page/home/home.jsx'
+import Canvas from './Components/page/canvas/Canvas.js'
+import ToolSwitcher from './Components/Tools/ToolSwitcher.js'
 import './App.css'
 
 function App(props) {
   return (
     <Router>
+    {/* <Line /> */}
+    {/* <Rectangle /> */}
+    {/* <Circle /> */}
+    <ToolSwitcher />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/canvas' element={<Canvas />} />
       </Routes>
     </Router>
-  )
-}
-
-<<<<<<< HEAD
-export default App
-=======
-      <Fragment>
-        <Header />
-        {/* <Line /> */}
-        {/* <Rectangle /> */}
-        {/* <Circle /> */}
-        <ToolSwitcher />
-      </Fragment>
-    
-
     )
   }
 
 export default App;
->>>>>>> just-canvas
