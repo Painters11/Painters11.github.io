743026fc4791d8b925b64cd6c51102d27c4a85f8
greg goldman <gregory.goldman97@gmail.com>
2021-11-20 10:57:47 -0500

    Cleans up

--- src/Components/page/sketch/Canvas/Draw.js
+++ src/Components/page/sketch/Canvas/Draw.js
@@ -90,7 +90,6 @@ const Draw = (props) => {
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
             ref={canvasRef}
-            
             >
             Paint
             
