e1699009f8041fc65b1fd4097458ab462f64e421
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-22 22:01:52 -0500

    - Created the team page and component - Created social media icons folder for easier imports

--- src/Components/shared/Team/index.jsx
+++ src/Components/shared/Team/index.jsx
@@ -1,53 +1,78 @@
 import React from 'react'
 import CardGroup from 'react-bootstrap/CardGroup'
 import Card from 'react-bootstrap/Card'
+import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
-const members = [
-   {
-    image:'',
-    firstName: 'Ilia',
-    lastName: 'de Leon',
-    role: 'Designer',
-    portfolio: 'https://www.iliadeleon.com/',
-    twitter: '',
-    github: 'https://github.com/iliadeleon',
-    linkedIn: 'https://www.linkedin.com/in/iliadeleon/'
-  },
-  {
-    image:'',
-    firstName: 'Greg',
-    lastName: 'Goldman',
-    role: 'Engineer',
-    portfolio: '',
-    twitter: '',
-    github: 'https://github.com/greggoldman11',
-    linkedIn: 'https://www.linkedin.com/in/greggoldman1/'
-  },
-  {
-    image:'',
-    firstName: 'Matt',
-    lastName: 'Moore',
-    role: 'Engineer',
-    portfolio: 'https://mattm.page/',
-    twitter: '@Matt_MMoore',
-    github: 'https://github.com/mmmoore1313',
-    linkedIn: 'https://www.linkedin.com/in/matt-m-moore-130013/'
-  }
-]
-
-function Team(props) {
-  const members = props.members members.map((member) =>
-  {member} )
+function Team() {
+  const members = [
+    {
+      id: 1,
+      image:'',
+      firstName: 'Ilia',
+      lastName: 'de Leon',
+      role: 'UX Designer',
+      portfolio: 'https://www.iliadeleon.com/',
+      twitter: '',
+      github: 'https://github.com/iliadeleon',
+      linkedIn: 'https://www.linkedin.com/in/iliadeleon/',
+    },
+    {
+      id: 2,
+      image:'',
+      firstName: 'Greg',
+      lastName: 'Goldman',
+      role: 'Software Engineer',
+      portfolio: '',
+      twitter: '',
+      github: 'https://github.com/greggoldman11',
+      linkedIn: 'https://www.linkedin.com/in/greggoldman1/',
+    },
+    {
+      id: 3,
+      image:'',
+      firstName: 'Matt',
+      lastName: 'Moore',
+      role: 'Software Engineer',
+      portfolio: 'https://mattm.page/',
+      twitter: '@Matt_MMoore',
+      github: 'https://github.com/mmmoore1313',
+      linkedIn: 'https://www.linkedin.com/in/matt-m-moore-130013/',
+    },
+  ]
   return (
     <CardGroup>
-      <Card>
+      {members.map((member) => (
+      <Card item key={member.id}>
         <Card.Img variant="top" src={member.image} />
         <Card.Body>
           <Card.Title>
             {member.firstName} {member.lastName}
           </Card.Title>
+          <Card.Text>
+            {member.role}
+          </Card.Text>
+          <Card.Footer>
+            <ul>
+              <li>
+                <MDBBtn className='m-1' style={{ backgroundColor: '#55acee' }} href={member.twitter}>
+                  <MDBIcon fab icon='twitter' />
+                </MDBBtn>
+              </li>
+              <li>
+                <MDBBtn className='m-1' style={{ backgroundColor: '#333333' }} href={member.github}>
+                  <MDBIcon fab icon='github' />
+                </MDBBtn>
+              </li>
+              <li>
+                <MDBBtn className='m-1' style={{ backgroundColor: '#0082ca' }} href={member.linkedIn}>
+                  <MDBIcon fab icon='linkedin-in' />
+                </MDBBtn>
+              </li>
+            </ul>
+          </Card.Footer>
         </Card.Body>
       </Card>
+    ))}
     </CardGroup>
   )
 }
