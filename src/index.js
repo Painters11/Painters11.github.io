439417919182600be019a3b4422519b6b5939ec9
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-23 15:28:35 -0500

    working towards a build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import _ from 'lodash'
 import App from './App.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
