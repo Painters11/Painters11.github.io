f1fb67a1c1c96675c1b3865f74871cb3ac9cebf8
Matt Moore <Matt.M.Moore13@outlook.com>
2021-11-24 12:15:50 -0500

    used my changes to the header index and style pages

--- src/Components/shared/Header/index.jsx
+++ src/Components/shared/Header/index.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
+import { MDBNavbar, MDBContainer, MDBIcon, MDBNavbarNav, MDBNavbarItem, MDBNavbarLink, MDBNavbarToggler, MDBCollapse, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem, MDBDropdownLink, MDBNavbarBrand } from 'mdb-react-ui-kit'
 import Image from 'react-bootstrap/Image'
 import logo from '../../../imgs/NavbarLogo.png'
 
 function Header() {
+  const [showNavRight, setShowNavRight] = useState(false)
+  
   return (
-<>
-      <Navbar bg="light" expand='md' id='nav' >
-        <Container fluid>
-          <Navbar.Brand>
-            <NavLink to="/" className='nav-link'>
-              <Image src={logo} />
-            </NavLink>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
+    <MDBNavbar bgColor="light" expand='lg' id='nav' >
+      <MDBContainer fluid>
+        <MDBNavbarBrand>
+          <NavLink to="/" className='nav-link'>
+            <Image src={logo} />
+          </NavLink>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarRightAlignExample'
+          aria-controls='navbarRightAlignExample'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavRight(!showNavRight)}
+          right
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavRight} id='menu'>
+          <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+            <MDBNavbarItem>
               <NavLink to="/team" className='nav-link'>Team</NavLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
               <NavLink to="/canvas" className='nav-link'>Draw</NavLink>
-              <Nav.Link href="/coloringbook">Coloring Book</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <NavLink to="/coloringbook" className='nav-link'>Coloring Book</NavLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   )
 }
 
